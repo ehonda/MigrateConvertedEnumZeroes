95b35a725a69cefd176c46dbaac924baae5f60bc
Dennis Renz <dennis@r-renz.de>
2022-03-04 20:54:25 +0100

    Add initial data seeding migration

---
+++ MigrateConvertedEnumZeroes/Program.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+return;
+
+internal class Movie
+{
+    public Movie(string title) => Title = title;
+    
+    public int Id { get; set; }
+
+    public string Title { get; set; }
+}
+
+internal class MoviesDbContext : DbContext
+{
+    public DbSet<Movie> Movies => Set<Movie>();
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlite("DataSource=movies.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Movie>().HasData(
+            new Movie("Some Cartoon") { Id = 1 },
+            new Movie("A Documentary") { Id = 2 });
+    }
+}
